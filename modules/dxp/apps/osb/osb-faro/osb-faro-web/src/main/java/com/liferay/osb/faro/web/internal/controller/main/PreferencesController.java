a8742b2a35f72d43d671f1c45c0753edce0c8497
Thiago Buarque <thiago.buarque@liferay.com>
2025-10-22 16:02:43 -0300

    LPD-68769 update existing endpoint to receive a list of ids

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/main/PreferencesController.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/main/PreferencesController.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.security.RolesAllowed;
@@ -245,8 +246,8 @@ public class PreferencesController extends BaseFaroController {
 			individualSegmentId);
 	}
 
-	public void removeIndividualSegmentPreferences(
-			long groupId, String individualSegmentId, String scope)
+	public void removeIndividualSegmentsPreferences(
+			long groupId, List<String> individualSegmentIds, String scope)
 		throws Exception {
 
 		long ownerId = _getOwnerId(groupId, scope);
@@ -254,8 +255,8 @@ public class PreferencesController extends BaseFaroController {
 		WorkspacePreferences workspacePreferences = _getWorkspacePreferences(
 			groupId, ownerId);
 
-		workspacePreferences.removeIndividualSegmentPreference(
-			individualSegmentId);
+		workspacePreferences.removeIndividualSegmentsPreferences(
+			individualSegmentIds);
 
 		_faroPreferencesLocalService.savePreferences(
 			getUserId(), groupId, ownerId,
