989c2b63173bc22422b1101ba7b19e8e8314980d
Marcos Martins <marcos.martins@liferay.com>
2025-12-17 18:00:17 -0300

    LPD-0 fix compile

--- modules/dxp/apps/osb/osb-faro/osb-faro-mock-engine-client/src/main/java/com/liferay/osb/faro/mock/engine/client/internal/BaseMockContactsEngineClientImpl.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-mock-engine-client/src/main/java/com/liferay/osb/faro/mock/engine/client/internal/BaseMockContactsEngineClientImpl.java
@@ -7,7 +7,6 @@ package com.liferay.osb.faro.mock.engine.client.internal;
 
 import com.liferay.osb.faro.engine.client.BaseEngineClient;
 import com.liferay.osb.faro.engine.client.ContactsEngineClient;
-import com.liferay.osb.faro.engine.client.constants.FieldMappingConstants;
 import com.liferay.osb.faro.engine.client.exception.FaroEngineClientException;
 import com.liferay.osb.faro.engine.client.model.Account;
 import com.liferay.osb.faro.engine.client.model.Activity;
@@ -704,15 +703,6 @@ public abstract class BaseMockContactsEngineClientImpl
 		return contactsEngineClient.getFieldValues(
 			faroProject, channelId, query, fieldMappingFieldName, cur, delta);
 	}
-
-	@Override
-	public Results<Object> getAccountFieldValues(
-		FaroProject faroProject, Long channelId,
-		 String fieldMappingFieldName, String query, int cur, int delta) {
-		return contactsEngineClient.getAccountFieldValues(
-			faroProject, channelId, fieldMappingFieldName, query, cur, delta);
-	}
-
 
 	@Override
 	public Individual getIndividual(
