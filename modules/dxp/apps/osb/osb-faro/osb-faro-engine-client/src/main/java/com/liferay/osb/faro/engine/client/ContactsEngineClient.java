289996328773411260f9959cdd6caefe681d5e52
Caio Elias Rabelo Pinheiro <caio.elias@liferay.com>
2025-12-17 10:58:15 -0300

    LPD-67819 method rename and SF

--- modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/ContactsEngineClient.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/ContactsEngineClient.java
@@ -269,6 +269,9 @@ public interface ContactsEngineClient {
 	public Results<DataSource> getDataSources(
 		FaroProject faroProject, List<String> channelIds);
 
+	public List<DataSource> getDataSources(
+		FaroProject faroProject, long channelId, String provideTypeExclude);
+
 	public Results<DataSource> getDataSources(
 		FaroProject faroProject, String faroEntityId, String query, String name,
 		String providerType, List<String> states, int cur, int delta,
@@ -446,9 +449,6 @@ public interface ContactsEngineClient {
 
 	public long getSalesforceUsersCount(
 		String dataSourceId, FaroProject faroProject);
-
-	public List<DataSource> getSegmentDataSources(
-		FaroProject faroProject, long channelId, String provideTypeExclude);
 
 	public Results<String> getSessionValues(
 		FaroProject faroProject, String channelId, String fieldName,
--- modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
@@ -1268,6 +1268,22 @@ public class ContactsEngineClientImpl
 	}
 
 	@Override
+	public List<DataSource> getDataSources(
+		FaroProject faroProject, long channelId, String provideTypeExclude) {
+
+		Map<String, Object> uriVariables = getUriVariables(faroProject);
+
+		uriVariables.put("channelId", channelId);
+		uriVariables.put("provideTypeExclude", provideTypeExclude);
+
+		return get(
+			faroProject, Rels.DATA_SOURCE_CHANNEL_CONNECTED,
+			new ParameterizedTypeReference<>() {
+			},
+			uriVariables);
+	}
+
+	@Override
 	public Results<DataSource> getDataSources(
 		FaroProject faroProject, String faroEntityId, String query, String name,
 		String providerType, List<String> states, int cur, int delta,
@@ -2441,22 +2457,6 @@ public class ContactsEngineClientImpl
 		}
 
 		return responseEntity.getBody();
-	}
-
-	@Override
-	public List<DataSource> getSegmentDataSources(
-		FaroProject faroProject, long channelId, String provideTypeExclude) {
-
-		Map<String, Object> uriVariables = getUriVariables(faroProject);
-
-		uriVariables.put("channelId", channelId);
-		uriVariables.put("provideTypeExclude", provideTypeExclude);
-
-		return get(
-			faroProject, Rels.DATA_SOURCE_CHANNEL_CONNECTED,
-			new ParameterizedTypeReference<>() {
-			},
-			uriVariables);
 	}
 
 	@Override
