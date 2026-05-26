d5dfde2e2a2b8b44f2085c7ad298823201388d5b
Caio Pinheiro <caio.elias@liferay.com>
2026-05-26 15:51:54 -0300

    LPD-90635 send channelId on request

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/api/accounts.js
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/api/accounts.js
@@ -69,8 +69,9 @@ export async function fetchLifecycleStatus({
 	});
 }
 
-export function fetchMetrics({groupId}) {
+export function fetchMetrics({channelId, groupId}) {
 	return sendRequest({
+		data: {channelId},
 		method: 'GET',
 		path: `contacts/${groupId}/account/metrics`
 	});
