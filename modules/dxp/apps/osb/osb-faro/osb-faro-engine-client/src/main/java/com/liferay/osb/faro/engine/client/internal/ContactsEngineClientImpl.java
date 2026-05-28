1384452d5bd01899b30a5c3ecd13479b4257fd2e
Caio Pinheiro <caio.elias@liferay.com>
2026-05-28 14:27:05 -0300

    LPD-92517 add channelId to individuals request

--- modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
@@ -656,13 +656,17 @@ public class ContactsEngineClientImpl
 
 	@Override
 	public Results<Individual> getAccountIndividuals(
-		FaroProject faroProject, String accountId, String query, int cur,
-		int delta, String sortString) {
+		FaroProject faroProject, String accountId, String channelId,
+		String query, int cur, int delta, String sortString) {
 
 		Map<String, Object> uriVariables = getUriVariables(
 			faroProject, cur, delta, null);
 
 		uriVariables.put("id", accountId);
+
+		if (Validator.isNotNull(channelId)) {
+			uriVariables.put("channelId", channelId);
+		}
 
 		if (Validator.isNotNull(query)) {
 			uriVariables.put("query", query);
